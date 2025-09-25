e5189e8f52092cb2bd74f703971b0dc2eb52cf9c
Abdullah <mdaaf.abdullah@gmail.com>
2025-09-25 23:14:56 +0600

    create-react-app to create a react project 1st way.

--- 01myreact/src/App.js
+++ 01myreact/src/App.js
@@ -1,24 +1,8 @@
-import logo from './logo.svg';
-import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+   <h1>I started REACT JOURNEY </h1>, 
+   <h2> so i created my react project for the first time, this is very hectic i swear, i am following hitesh chowdhury and sumit and also hm nayeem in this journey .first hitesh vedio i saw and my nodejs and node package manager is maybe some issue in this, first day i didnt understand anything ,then 2nd day i follow sumit ,but his old vedios also deag my brain, and then 2nd day i helped with grokAI and create step by step proccess what problem in my laptop or my folder instructions,so some problem i fixed with the help of grokAI ,and here its maybe i understand how create react app and slo how git command give  </h2>
   );
 }
 
